1aa0b92db1050bef65bbeb89acb64152af92e93d
矢野大樹 <daikitech0123@gmail.com>
2021-02-28 23:10:11 +0900

    add user

--- main.go
+++ main.go
@@ -18,11 +18,14 @@ func main() {
 
 	fmt.Println(models.Db)
 
-	u := &models.User{}
-	u.Name = "test"
-	u.Email = "test@gmail.com"
-	u.PassWord = "testtest"
+	// u := &models.User{}
+	// u.Name = "test"
+	// u.Email = "test@gmail.com"
+	// u.PassWord = "testtest"
+	// fmt.Println(u)
+	// u.CreateUser()
+
+	u, _ := models.GetUser(1)
 	fmt.Println(u)
-	u.CreateUser()
 
 }
