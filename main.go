6033bb8e6ae26ec8982ecef13164452046697509
矢野大樹 <daikitech0123@gmail.com>
2021-03-05 12:02:42 +0900

    サーバーテスト

--- main.go
+++ main.go
@@ -2,49 +2,13 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"todo_app/app/controllers"
 	"todo_app/app/models"
 )
 
 func main() {
 
-	log.Println("test")
-
 	fmt.Println(models.Db)
-
-	// u := &models.User{}
-	// u.Name = "test2"
-	// u.Email = "test2@gmail.com"
-	// u.PassWord = "testtest"
-	// fmt.Println(u)
-	// u.CreateUser()
-
-	// u, _ := models.GetUser(1)
-	// fmt.Println(u)
-
-	// u.Name = "Test2"
-	// u.Email = "test2@gmail.com"
-	// u.UpdateUser()
-	// u, _ = models.GetUser(1)
-	// fmt.Println(u)
-
-	// u.DeleteUser()
-	// user, _ := models.GetUser(3)
-	// user.CreateTodo("third Todo")
-	// fmt.Println(user)
-	todos, _ := models.GetTodos()
-	for _, v := range todos {
-		fmt.Println(v)
-	}
-	// user2, _ := models.GetUser(3)
-	// todos, _ := user2.GetTodosByUser()
-	// for _, v := range todos {
-	// 	fmt.Println(v)
-	// }
-	// t, _ := models.GetTodo(3)
-	// t.Content = "Update todo"
-	// t.UpdateTodo()
-	// fmt.Println(t)
-	// t.DeleteTodo()
+	controllers.StartMainServer()
 
 }
