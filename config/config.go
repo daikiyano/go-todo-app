2d2a44896b7ee3b1b5034598c197cdb04f8666db
矢野大樹 <daikitech0123@gmail.com>
2021-03-06 22:49:30 +0900

    add config

--- config/config.go
+++ config/config.go
@@ -12,6 +12,7 @@ type ConfigList struct {
 	SQLDriver string
 	DbName    string
 	LogFile   string
+	Static    string
 }
 
 var Config ConfigList
@@ -34,6 +35,7 @@ func LoadConfig() {
 		SQLDriver: cfg.Section("db").Key("driver").String(),
 		DbName:    cfg.Section("db").Key("name").String(),
 		LogFile:   cfg.Section("web").Key("logfile").String(),
+		Static:    cfg.Section("web").Key("static").String(),
 	}
 
 }
