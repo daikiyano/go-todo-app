91529d75b48c2eb48913b93993fe7560eef67f18
矢野大樹 <daikitech0123@gmail.com>
2021-03-12 21:52:06 +0900

    add layout

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -1,15 +1,14 @@
 package controllers
 
 import (
-	"html/template"
-	"log"
 	"net/http"
 )
 
 func top(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("app/views/templates/top.html")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	t.Execute(w, nil)
+	generateHTML(w, "hello", "layout", "top")
+	// t, err := template.ParseFiles("app/views/templates/top.html")
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+	// t.Execute(w, nil)
 }
